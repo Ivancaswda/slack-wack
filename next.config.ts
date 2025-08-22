6c749a77a59e49a63a832ff250b49fab2311bf9e
aivanius <aivanius@yandex.ru>
2025-08-22 23:02:12 +0700

    asgasgas

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
       domains: ['neat-dove-689.convex.cloud']
-  }
+  },
+    eslint: {
+        ignoreDuringBuilds: true, // отключает ESLint при сборке
+    },
+    typescript: {
+        ignoreBuildErrors: true, // отключает ошибки TypeScript при сборке
+    },
+
 };
 
 export default nextConfig;
